94f08db3fe4a0e5a56630618cf2c7acc5aa8c02b
BabaGl <gallebaba71@gmail.com>
2023-06-06 12:21:32 +0000

    import Module

--- src/app/externe/externe.module.ts
+++ src/app/externe/externe.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExterneRoutingModule } from './externe-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ExterneRoutingModule
+    ExterneRoutingModule,
+    SharedModule
   ]
 })
 export class ExterneModule { }
--- src/app/interne/interne.module.ts
+++ src/app/interne/interne.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InterneRoutingModule } from './interne-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    InterneRoutingModule
+    InterneRoutingModule,
+    SharedModule
   ]
 })
 export class InterneModule { }
