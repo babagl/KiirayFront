d884e8b92a4af4b56c34e375219a176b98ab8248
BabaGl <gallebaba71@gmail.com>
2023-06-06 12:08:13 +0000

    first commit

---
+++ src/app/interne/interne.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InterneRoutingModule } from './interne-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InterneRoutingModule
+  ]
+})
+export class InterneModule { }
